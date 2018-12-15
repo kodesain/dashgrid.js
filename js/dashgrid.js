6e18b8064c6c15e4f2773d9197be58166f859389
Romi A Hakim <romi.hakim@eaciit.com>
2018-12-15 08:10:46 +0700

    add collision and resizable function

--- js/dashgrid.js
+++ js/dashgrid.js
@@ -1,7 +1,9 @@
+"use strict";
+
 var dashGrid = function (el) {
     this.grid = $(el);
-    this.cols = 3
-    this.rows = parseInt(Math.ceil(this.gridItem().length / this.cols));
+    this.cols = 4;
+    this.rows = parseFloat(Math.ceil(this.gridItem().length / this.cols));
     this.nodes = {};
     this.item = {
         width: (100 / this.cols),
@@ -15,8 +17,8 @@ var dashGrid = function (el) {
 dashGrid.prototype.hintNodes = function (row) {
     var last = (typeof row !== "undefined") ? row : this.rows;
 
-    for (r = 0; r < (last + 1); r++) {
-        for (c = 0; c < this.cols; c++) {
+    for (var r = 0; r < (last + 1); r++) {
+        for (var c = 0; c < this.cols; c++) {
             var top = this.item.height * r;
             var left = this.item.width * c;
 
@@ -25,6 +27,7 @@ dashGrid.prototype.hintNodes = function (row) {
     }
 
     this.rows = last;
+    this.gridHeight();
 
     return this.nodes;
 };
@@ -35,6 +38,10 @@ dashGrid.prototype.gridItem = function (attr) {
 
 dashGrid.prototype.gridHint = function (attr) {
     return $("#" + this.grid.get(0).id + " > .dash-grid-hint" + (typeof attr !== "undefined" ? attr : ""));
+};
+
+dashGrid.prototype.gridHeight = function () {
+    this.grid.css({"position": "relative", "height": (this.item.height * (this.rows + 1)) + "px"});
 };
 
 dashGrid.prototype.mapItems = function () {
@@ -55,8 +62,8 @@ dashGrid.prototype.mapItems = function () {
 
     var i = 0;
 
-    for (r = 0; r < this.rows; r++) {
-        for (c = 0; c < this.cols; c++) {
+    for (var r = 0; r < this.rows; r++) {
+        for (var c = 0; c < this.cols; c++) {
             if (typeof exist[i] !== "undefined") {
                 if (typeof this.gridItem("[data-row='" + r + "'][data-col='" + c + "']").get(0) === "undefined") {
                     this.gridItem("[data-id='" + exist[i] + "']").attr("data-row", r).attr("data-col", c).css(this.nodes[r + "," + c ]);
@@ -66,40 +73,7 @@ dashGrid.prototype.mapItems = function () {
         }
     }
 
-    this.grid.css({"position": "relative", "height": (this.item.height * (this.rows + 1)) + "px"});
     this.grid.append('<div class="dash-grid-hint"></div>');
-};
-
-dashGrid.prototype.resizeItem = function (el) {
-    var style = this.getStyles(el);
-    var attr = this.getAttributes(el);
-    var mode = (typeof attr["data-mode"] !== "undefined") ? attr["data-mode"] : "min";
-
-    if (mode == "min") {
-        mode = "max";
-        style.width = (parseInt(style.width) * 2) + "%";
-        style.height = (parseInt(style.height) * 2) + "px";
-
-        if (parseInt(attr["data-col"]) == (this.cols - 1)) {
-            $(el).attr("data-col", (this.cols - 2));
-            $(el).attr("data-x-col", (this.cols - 1));
-
-            style.left = this.item.width * (this.cols - 2) + "%";
-        }
-    } else {
-        mode = "min";
-        style.width = (parseInt(style.width) / 2) + "%";
-        style.height = (parseInt(style.height) / 2) + "px";
-
-        if (typeof attr["data-x-col"] !== "undefined") {
-            $(el).attr("data-col", $(el).attr("data-x-col"));
-            $(el).removeAttr("data-x-col");
-
-            style.left = this.item.width * (this.cols - 1) + "%";
-        }
-    }
-
-    $(el).attr("data-mode", mode).css(style);
 };
 
 dashGrid.prototype.getData = function (el) {
@@ -156,16 +130,22 @@ dashGrid.prototype.draggable = function () {
             var drag = $(".dash-grid-item.drag").get(0);
             var dragData = self.getData(drag);
             var dragStyles = self.getStyles(drag);
-            var dragY = (parseInt(dragStyles.top) - self.grid.offset().top) + e.offsetY;
-            var dragX = (parseInt(dragStyles.left) - self.grid.offset().left) + e.offsetX;
-            var dragTop = parseInt(dragY);
-            var dragLeft = parseInt((dragX / self.grid.width()) * 100);
+            var dragY = (parseFloat(dragStyles.top) - self.grid.offset().top) + e.offsetY;
+            var dragX = (parseFloat(dragStyles.left) - self.grid.offset().left) + e.offsetX;
+            var dragTop = parseFloat(dragY);
+            var dragLeft = parseFloat((dragX / self.grid.width()) * 100);
+            var dragMode = (typeof dragData["mode"] !== "undefined") ? dragData["mode"] : "min";
 
             $.each(self.nodes, function (key, node) {
-                if ((parseInt(dragTop) >= parseInt(node.top) && parseInt(dragTop) <= (parseInt(node.top) + parseInt(node.height)))) {
-                    if ((parseInt(dragLeft) >= parseInt(node.left) && parseInt(dragLeft) <= (parseInt(node.left) + parseInt(node.width)))) {
-                        var r = parseInt(key.split(",")[0]);
-                        var c = parseInt(key.split(",")[1]);
+                if ((parseFloat(dragTop) >= parseFloat(node.top) && parseFloat(dragTop) <= (parseFloat(node.top) + parseFloat(node.height)))) {
+                    if ((parseFloat(dragLeft) >= parseFloat(node.left) && parseFloat(dragLeft) <= (parseFloat(node.left) + parseFloat(node.width)))) {
+                        var r = parseFloat(key.split(",")[0]);
+                        var c = parseFloat(key.split(",")[1]);
+
+                        if (dragMode == "max") {
+                            node.width = (self.item.width * 2) + "%";
+                            node.height = (self.item.height * 2) + "px";
+                        }
 
                         self.gridHint().attr("data-row", r).attr("data-col", c).css(node);
                     }
@@ -176,36 +156,41 @@ dashGrid.prototype.draggable = function () {
             var hint = self.gridHint().get(0);
             var hintAttr = self.getAttributes(hint);
             var hintStyles = self.getStyles(hint);
-            var hintRow = parseInt(hintAttr["data-row"]);
-            var hintCol = parseInt(hintAttr["data-col"]);
+            var hintRow = parseFloat(hintAttr["data-row"]);
+            var hintCol = parseFloat(hintAttr["data-col"]);
             var hintMode = hintAttr["data-mode"];
 
             var drag = e.currentTarget.get(0);
             var dragAttr = self.getAttributes(drag);
             var dragStyles = self.getStyles(drag);
-            var dragRow = parseInt(dragAttr["data-row"]);
-            var dragCol = parseInt(dragAttr["data-col"]);
+            var dragRow = parseFloat(dragAttr["data-row"]);
+            var dragCol = parseFloat(dragAttr["data-col"]);
             var dragMode = dragAttr["data-mode"];
 
             var drop = self.gridItem("[data-row='" + hintRow + "'][data-col='" + hintCol + "']").get(0);
             var dropAttr = self.getAttributes(drop);
             var dropStyles = self.getStyles(drop);
-            var dropRow = parseInt(dropAttr["data-row"]);
-            var dropCol = parseInt(dropAttr["data-col"]);
+            var dropRow = parseFloat(dropAttr["data-row"]);
+            var dropCol = parseFloat(dropAttr["data-col"]);
             var dropMode = dropAttr["data-mode"];
 
-            if (typeof drop !== "undefined") {
-                $(drop).attr("data-row", dragRow).attr("data-col", dragCol).css(dragStyles);
-            }
-
+            //if (dragMode == dropMode) {
             $(drag).attr("data-row", hintRow).attr("data-col", hintCol).css(hintStyles);
+            //}
+
+            if (typeof drop !== "undefined") {
+                if (dragMode == dropMode) {
+                    $(drop).attr("data-row", dragRow).attr("data-col", dragCol).css(dragStyles);
+                }
+            }
 
             if (hintRow === self.rows) {
                 self.hintNodes(hintRow + 1);
-                self.grid.css({"position": "relative", "height": (self.item.height * (self.rows + 1)) + "px"});
             }
 
+            self.collision(drag);
             self.gridHint().removeAttr("style");
+
             $(".dash-grid-item.drag").hide();
         }
     });
@@ -213,6 +198,107 @@ dashGrid.prototype.draggable = function () {
     this.grid.on("click", ".resize", function (e) {
         var el = $(e.currentTarget).parent().get(0);
 
-        self.resizeItem(el);
+        self.resizable(el);
+    });
+};
+
+dashGrid.prototype.resizable = function (el) {
+    var self = this;
+    var style = this.getStyles(el);
+    var attr = this.getAttributes(el);
+    var mode = (typeof attr["data-mode"] !== "undefined") ? attr["data-mode"] : "min";
+
+    if (mode == "min") {
+        mode = "max";
+        style.width = (parseFloat(style.width) * 2) + "%";
+        style.height = (parseFloat(style.height) * 2) + "px";
+
+        if (parseFloat(attr["data-col"]) == (this.cols - 1)) {
+            $(el).attr("data-col", (this.cols - 2));
+            $(el).attr("data-x-col", (this.cols - 1));
+
+            style.left = this.item.width * (this.cols - 2) + "%";
+        }
+    } else {
+        mode = "min";
+        style.width = (parseFloat(style.width) / 2) + "%";
+        style.height = (parseFloat(style.height) / 2) + "px";
+
+        if (typeof attr["data-x-col"] !== "undefined") {
+            $(el).attr("data-col", $(el).attr("data-x-col"));
+            $(el).removeAttr("data-x-col");
+
+            style.left = this.item.width * (this.cols - 1) + "%";
+        }
+    }
+
+    $(el).attr("data-mode", mode).css(style);
+
+    if (mode == "min") {
+        this.gridItem().each(function (i, e) {
+            var x = $(e).attr("data-x-row");
+            var s = self.getStyles(e);
+
+            if (typeof x !== "undefined") {
+                s.top = self.item.height * x;
+
+                $(e).attr("data-row", x).css(s);
+                $(e).removeAttr("data-x-row");
+            }
+        });
+    } else {
+        this.gridItem().each(function (i, e) {
+            $(e).removeAttr("data-x-row");
+        });
+    }
+
+    this.collision(el);
+};
+
+dashGrid.prototype.collision = function (el) {
+    var self = this;
+    var main = this.getAttributes(el);
+    var mainStyles = this.getStyles(el);
+    var mainMode = (typeof main["data-mode"] !== "undefined") ? main["data-mode"] : "min";
+
+    var ee = [];
+
+    for (var r = 0; r < this.rows; r++) {
+        for (var c = 0; c < this.cols; c++) {
+            this.gridItem("[data-row='" + r + "'][data-col='" + c + "']").each(function (i, e) {
+                var other = self.getAttributes(e);
+                var otherStyles = self.getStyles(e);
+
+                if (other["data-id"] != main["data-id"]) {
+                    if ((parseFloat(otherStyles.top) >= parseFloat(mainStyles.top) && parseFloat(otherStyles.top) < (parseFloat(mainStyles.top) + parseFloat(mainStyles.height)))) {
+                        if ((parseFloat(otherStyles.left) >= parseFloat(mainStyles.left) && parseFloat(otherStyles.left) < (parseFloat(mainStyles.left) + parseFloat(mainStyles.width)))) {
+                            var row = parseFloat(other["data-row"]) + 1;
+
+                            if (mainMode == "max" && parseFloat(other["data-row"]) == parseFloat(main["data-row"])) {
+                                row = parseFloat(other["data-row"]) + 2;
+                            }
+
+                            otherStyles.top = self.item.height * row;
+
+                            if (typeof other["data-x-row"] !== "undefined") {
+                                $(e).attr("data-row", row).css(otherStyles);
+                            } else {
+                                $(e).attr("data-row", row).attr("data-x-row", parseFloat(other["data-row"])).css(otherStyles);
+                            }
+
+                            ee.push($(e));
+
+                            if (row === self.rows) {
+                                self.hintNodes(row + 1);
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    $(ee).each(function (i, e) {
+        self.collision(e);
     });
 };
